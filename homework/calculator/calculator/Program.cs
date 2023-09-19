535ed0c653dde3b60247f0a84fc91de1a1d8c7a5
cenekzahrobsky <cenek.zahrobsky@skaut.cz>
2023-09-19 11:29:38 +0200

    switch,log,binary

--- homework/calculator/calculator/Program.cs
+++ homework/calculator/calculator/Program.cs
@@ -7,73 +7,88 @@ using System.Threading.Tasks;
 namespace calculator
 {
     internal class Program
+        
     {
         static void Main(string[] args)
         {
+            string binary = null;
             Console.WriteLine("Hello, World!");
 
             while (true)
             {
                 Console.WriteLine("Vyberte operaci");
-                Console.WriteLine("1 - secti, 2 - odecti, 3 - vynasob, 4 - vydel, 5 - na druhou, 6 - odmocni");
+                Console.WriteLine("1 - scitani, 2 - odcitani, 3 - nasobeni, 4 - deleni, 5 - mocnina a na b, 6 - druha mocnina, 7 - odmocnina, 8 - logaritmus, 9 - binarni kod");
                 int operation = int.Parse(Console.ReadLine());
                 Console.WriteLine();
-
+               
                 Console.WriteLine("Zadej a");
                 int a = int.Parse(Console.ReadLine());
                 Console.WriteLine();
-
+                
                 int b = 0;
-                if (operation < 5)
+                if (operation <6)
                 {
                     Console.WriteLine("Zadej b");
                     b = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
                 }
-
-
-
 
                 double result = 0.0;
 
-                if (operation == 1)
+                switch (operation)
                 {
-                    result = a + b;
-                }
-                else if (operation == 2)
-                {
-                    result = a - b;
-                }
-                else if (operation == 3)
-                {
-                    result = a * b;
-                }
-                else if (operation == 4)
-                {
-                    if (b == 0)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Nesmite delit 0");
-                        continue;
-                    }
+                    case 1: 
+                        result = a + b;
+                        break;
+                    
+                    case 2: 
+                        result = a - b;
+                        break;
 
-                    result = Convert.ToDouble(a) / b;
-                }
-                else if (operation == 5)
-                {
-                    result = Math.Pow(a, 2);
-                }
-                else if (operation == 6)
-                {
-                    result = Math.Sqrt(a);
-                }
-                else
-                {
-                    Console.WriteLine("Nevalidni cislo operace");
-                    continue;
-                }
+                    case 3: 
+                        result = a * b;
+                        break;
 
-                Console.WriteLine("Vysledek je");
-                Console.WriteLine(result);
+                    case 4:
+                        if (b == 0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Nesmite delit 0");
+                            continue;
+                        }
+                        result = Convert.ToDouble(a) / b;
+                        break;
+
+                    case 5: 
+                        result = Math.Pow(a, b);
+                        break;
+
+                    case 6: 
+                        result = Math.Pow(a, 2);
+                        break;
+
+                    case 7: 
+                        result = Math.Sqrt(a);
+                        break;
+
+                    case 8: 
+                        result = Math.Log(a);
+                        break;
+
+                    case 9:
+                        binary = Convert.ToString(a, 2);
+                        break;
+
+                    default:
+                        Console.WriteLine("Nevalidni operace");
+                        break;
+
+                }
+                if (operation < 9)
+                Console.WriteLine("Vysledek je: "+result);
+                
+                if (operation == 9)
+                    Console.WriteLine("cislo a v binarim kodu: "+ binary);
 
                 Console.WriteLine();
                 Console.WriteLine();
