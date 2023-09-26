d95789dec76eb30ae490f7c57e1cd7c91badd330
cenekzahrobsky <cenek.zahrobsky@skaut.cz>
2023-09-26 11:31:06 +0200

    detahroll

---
+++ lecture/Deathroll/Deathroll/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deathroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocetGoldu = 1000;
+            int sazka = int.Parse(Console.ReadLine());
+            if (sazka > pocetGoldu)
+                Console.WriteLine("nemas dostatek financi");
+            else
+            {
+                while (sazka > 1)
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ lecture/KamenNuzkyPapir/KamenNuzkyPapir/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamenNuzkyPapir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hrac = 0;
+            int pocitac = 0;
+            while (true)
+            {
+                Console.WriteLine("Vyberte akci, (pro ukonceni zvolte 4)");
+                Console.WriteLine("1 - kamen, 2 - nuzky, 3 - papir");
+                int akce = int.Parse(Console.ReadLine());
+                Random rnd = new Random();
+                int a = rnd.Next(3);
+                if (akce == 1)
+                {
+                    if (a == 1)
+                    {
+                        Console.WriteLine("Souper zvolil kamen, Remíza!");
+                    }
+                    else if (a == 2)
+                    {
+                        Console.WriteLine("Souper zvolil nuzky, Výhra!");
+                        hrac++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Souper zvolil papir, Prohra!");
+                        pocitac++;
+                    }
+                }
+                else if (akce == 2)
+                {
+                    if (a == 1)
+                    {
+                        Console.WriteLine("Souper zvolil kamen, Prohra!");
+                        pocitac++;
+                    }
+                    else if (a == 2)
+                    {
+                        Console.WriteLine("Souper zvolil nuzky, Remíza!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Souper zvolil papir, Vyhra!");
+                        hrac++;
+                    }
+                }
+                else if (akce == 3)
+                {
+                    if (a == 1)
+                    {
+                        Console.WriteLine("Souper zvolil kamen, Vyhra!");
+                        pocitac++;
+                    }
+                    else if (a == 2)
+                    {
+                        Console.WriteLine("Souper zvolil nuzky, Prohra!");
+                        hrac++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Souper zvolil papir, Remíza!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Konecne skore je: " + hrac + ":" + pocitac);
+                    Console.ReadKey();
+                    break;
+                    
+                }
+            }
+
+        }
+    }
+}
