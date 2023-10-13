9ba494c2659c3c0c8936a505ebd2a50acb9cbd14
cenekzahrobsky <cenek.zahrobsky@skaut.cz>
2023-10-13 10:59:36 +0200

    dos

---
+++ lecture/RecursionPlayground/RecursionPlayground/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
+ * Extended by students.
+ */
+
+namespace RecursionPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
+            long factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
+            int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
+            Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            Console.ReadKey();
+        }
+
+        static long Factorial(int n)
+        {
+            if (n < 1) throw new Exception("Faktorial z cisla mensiho nez 1");
+            else if (n == 1) return 1;
+            else return n * Factorial(n - 1);
+
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n > 2) return Fibonacci(n - 1) + Fibonacci(n - 2);
+            else if (n == 0) return 0;
+            else return 1;
+        }
+    }
+}
